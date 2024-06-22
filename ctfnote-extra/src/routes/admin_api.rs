120f2d89078b99687642d3a214e0379955bbef0f
J Cui <i@cjxol.com>
2024-06-22 21:18:21 +0100

    Implement create CTFNote account linked to Discord user

--- ctfnote-extra/src/routes/admin_api.rs
+++ ctfnote-extra/src/routes/admin_api.rs
@@ -2,9 +2,9 @@ use std::sync::Arc;
 
 use axum::{extract::State, http::StatusCode, routing::post, Json, Router};
 use jsonwebtoken::get_current_timestamp;
-use serde::{Deserialize, Serialize};
-use sqlx::PgPool;
 use rand::{self, thread_rng, RngCore};
+use serde::{Deserialize, Serialize};
+use sqlx::{postgres::PgQueryResult, PgPool};
 
 use crate::{config::config, AppState};
 
@@ -23,12 +23,25 @@ async fn get_user_by_token(pool: &PgPool, token: String) -> Option<i32> {
         .await.expect("Failed to get user id from token")
 }
 
-async fn get_user_by_discord_id(pool: &PgPool, discord_id: String) -> Option<i32> {
+async fn get_user_by_discord_id(pool: &PgPool, discord_id: &String) -> Option<i32> {
     sqlx::query_scalar("SELECT id FROM ctfnote.profile WHERE discord_id = $1 LIMIT 1")
-        .bind(&discord_id)
+        .bind(discord_id)
         .fetch_optional(pool)
         .await
         .expect("Failed to get user id from discord id")
+}
+
+async fn set_discord_id_for_user(
+    db_pool: &PgPool,
+    user_id: i32,
+    discord_id: &String,
+) -> PgQueryResult {
+    sqlx::query("UPDATE ctfnote.profile SET discord_id = $2 WHERE id = $1")
+        .bind(user_id)
+        .bind(discord_id)
+        .execute(db_pool)
+        .await
+        .expect("Failed to set discord id for user in the database")
 }
 
 #[derive(Deserialize)]
@@ -62,22 +75,17 @@ async fn link_discord(
 
     // update discord id for user
     let discord_id = link_discord_request.discord_id;
-    let result = sqlx::query("UPDATE ctfnote.profile SET discord_id = $2 WHERE id = $1")
-        .bind(user_id)
-        .bind(&discord_id)
-        .execute(db_pool)
-        .await
-        .expect("Failed to set discord id for user in the database");
+    let result = set_discord_id_for_user(db_pool, user_id, &discord_id).await;
     if result.rows_affected() != 1 {
         return (StatusCode::BAD_REQUEST, Json(LinkDiscordResponse {
-            message: "You can't link the same Discord account twice! Please use a different Discord account or investigate why your account is already linked.".to_string(),
+            message: "You can't link the same Discord account twice!".to_string(),
         }));
     }
     tracing::info!("Linked user {} to Discord user {}", user_id, discord_id);
     return (
         StatusCode::OK,
         Json(LinkDiscordResponse {
-            message: "Successfully linked your Discord account to your CTFNote account!"
+            message: "Successfully linked Discord account to CTFNote account!"
                 .to_string(),
         }),
     );
@@ -151,7 +159,7 @@ async fn generate_jwt_for_discord_user(
     let config = config();
     let discord_id = request.discord_id;
 
-    let result = get_user_by_discord_id(db_pool, discord_id).await;
+    let result = get_user_by_discord_id(db_pool, &discord_id).await;
     let Some(user_id) = result else {
         return (
             StatusCode::BAD_REQUEST,
@@ -225,41 +233,49 @@ async fn register_and_link_discord(
     let discord_id = request.discord_id;
 
     //TODO: make atomic
-    let user = get_user_by_discord_id(db_pool, discord_id).await;
+    let user = get_user_by_discord_id(db_pool, &discord_id).await;
     if user.is_some() {
         return (
             StatusCode::BAD_REQUEST,
             Json(RegisterAndLinkDiscordResponse {
-                message: "Discord account already linked to CTFNote account."
-                    .to_string(),
+                message: "Discord account already linked to CTFNote account.".to_string(),
             }),
-        )
+        );
     }
 
-    let mut random_password = vec![0;32];
+    let mut random_password = vec![0; 32];
     thread_rng().fill_bytes(&mut random_password);
-    let result: Result<Jwt, sqlx::Error> = sqlx::query_scalar("SELECT ctfnote_private.do_register($1, $2, 'user_guest')")
-        .bind(request.username)
-        .bind(format!("{:x?}", random_password))
-        .fetch_one(db_pool)
-        .await;
+    let result: Result<Jwt, sqlx::Error> =
+        sqlx::query_scalar("SELECT ctfnote_private.do_register($1, $2, 'user_guest')")
+            .bind(request.username)
+            .bind(format!("{:x?}", random_password))
+            .fetch_one(db_pool)
+            .await;
 
+    // if username exists
     if let Err(err) = result {
         return (
             StatusCode::BAD_REQUEST,
             Json(RegisterAndLinkDiscordResponse {
                 message: err.as_database_error().unwrap().message().to_string(),
-            })
-        )
+            }),
+        );
     }
 
-    //TODO: link discord account
-
+    let user_id = result.unwrap().user_id;
+    let result = set_discord_id_for_user(db_pool, user_id, &discord_id).await;
+    if result.rows_affected() != 1 {
+        return (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            Json(RegisterAndLinkDiscordResponse {
+                message: "Could not link Discord account. This is unexpected.".to_string(),
+            }),
+        );
+    }
     (
         StatusCode::OK,
         Json(RegisterAndLinkDiscordResponse {
-            message: "Successfully created CTFNote account!"
-                .to_string(),
+            message: "Successfully created CTFNote account!".to_string(),
         }),
     )
 }
